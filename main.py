a4bf3a6d6371924744f407c0e277bc33644ff553
Himanshu Singla <108661887+himanshu-911@users.noreply.github.com>
2022-07-17 21:20:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Import libraries
 import cv2
 import mediapipe as mp
 import pyautogui
@@ -8,14 +7,12 @@ cap = cv2.VideoCapture(0)
 hand_detector = mp.solutions.hands.Hands()
 drawing_utils = mp.solutions.drawing_utils
 
-# get the screen size
 screen_width, screen_height = pyautogui.size()
 
-# Reducing the shake in cursor by smoothening the movement
 smoothing=2.7
-# previous location
+
 plocX, plocY=0,0
-# current location
+
 clocX, clocY=0,0
 
 index_y = 0
@@ -30,21 +27,21 @@ while True:
         for hand in hands:
             drawing_utils.draw_landmarks(frame, hand)
             landmarks = hand.landmark
-            # to iterate each id and landmark one by one
+            
             for id, landmark in enumerate(landmarks):
-                # get the positon on x and y axis of the frame
+                
                 x = int(landmark.x*frame_width)
                 y = int(landmark.y*frame_height)
-                # marking circle on the tip of index and middle finger for moving of cursor
+                
                 if id == 8:
-                    # defining circle parameters
+                    
                     cv2.circle(frame, (x,y), 10, (0, 255, 255), cv2.FILLED)
-                    # calibratihg indices to move cursor on whole screen
+                    
                     index_x = screen_width/frame_width*x
                     index_y = screen_height/frame_height*y
                     clocX = plocX + (index_x - plocX) / smoothing
                     clocY = plocY + (index_y - plocY) / smoothing
-                    # for movement of cursor along with the movement of index finger
+                    
                     pyautogui.moveTo(clocX, clocY)
                     plocX, plocY = clocX, clocY
                 if id == 12:
@@ -52,7 +49,7 @@ while True:
                     middle_x = screen_width/frame_width*x
                     middle_y = screen_height/frame_height*y
                     print('Position', abs(index_y - middle_y))
-                    # if distance between index and middle finger is less than 50 pixels then it would be considered as click
+                    
                     if abs(index_y - middle_y) < 50:
                         print('Click!')
                         pyautogui.click()
